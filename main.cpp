75cbada202d3ea6f7e74a074c4302616f0db0caf
sanjiija <sanjiija@gmail.com>
2023-10-10 10:52:40 +0600

    #1: Реализован калькулятор на switch case

--- main.cpp
+++ main.cpp
@@ -4,8 +4,39 @@ int main() {
 
 	setlocale(LC_ALL, "RU");
 
-	std::cout << "hello mir \n";
-	std::cout << "манера крутит мир \n" << std::endl;
+	float num1, num2, res;
+	char math;
+
+	std::cout << "Введите число 1 = ";
+	std::cin >> num1;
+
+	std::cout << "Введите число 2 = ";
+	std::cin >> num2;
+
+	std::cout << "Введите математическое действие (+, -, *, /) = ";
+	std::cin >> math;
+
+	switch (math) {
+	case('+'):
+		res = num1 + num2; 
+		std::cout << "Результат = " << res;
+		break;
+	case('-'):
+		res = num1 - num2;
+		std::cout << "Результат = " << res;
+		break;
+	case('*'):
+		res = num1 * num2;
+		std::cout << "Результат = " << res;
+		break;
+	case('/'):
+		res = num1 / num2;
+		std::cout << "Результат = " << res;
+		break;
+
+	}
+
+
 
 	return 0;
 }
